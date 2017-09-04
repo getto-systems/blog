e8c146073d097f229cdd778ca54b8652b8703cf8
shun <shun@getto.systems>
2017-09-04 23:58:09 +0900

    fix docker-wrapper env

--- bin/docker-wrapper.rc.sh
+++
@@ -1,5 +0,0 @@
-docker_wrapper_map node 8.4.0
-
-docker_wrapper_server_env_livereload(){
-  docker_wrapper_env -p ${LABO_PORT_PREFIX}80:8000 -p ${LABO_PORT_PREFIX}29:${LABO_PORT_PREFIX}29 -e LABO_PORT_PREFIX=$LABO_PORT_PREFIX
-}
